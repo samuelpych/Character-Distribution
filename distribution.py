21d700470ef780d46321a095293c507197554289
samuelpych <samuelpych@hanovernorwichschools.org>
2017-03-21 14:56:01 -0400

    Updated from Brython Server: 3/21/2017 2:56:01 PM

--- distribution.py
+++ distribution.py
@@ -39,7 +39,7 @@ Notice about this example:
 import string
 alphabet=('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
 idk=input("Please enter a string of text (the bigger the better): ")
-print("The distrubution of characters in \"" +idk+ "\" is:")
+print("The distribution of characters in \"" +idk+ "\" is:")
 
 idk=idk.lower()
 bazinga=[]
